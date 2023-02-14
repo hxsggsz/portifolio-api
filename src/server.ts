0c68706ca8c162f27600800d693b82ba94f69b0b
victorhugo-js <vt.hugo.2021@gmail.com>
2023-02-13 22:29:31 -0300

    resolving cors error

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import Express from "express";
 import { router } from "./route";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const app = Express();
 
 app.use(Express.json());
 
+app.use(cors(corsOptions));
+
 app.use(router);
 
-app.listen(3333, () =>
-  console.log("Server is running on http://localhost:3333")
-);
+app.listen(3333, () => console.log("rodando em http://localhost:3333"));
